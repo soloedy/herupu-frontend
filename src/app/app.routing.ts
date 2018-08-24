d500720d76f26cca957ac9a3459a337819b030a2
soloedy <32673751+soloedy@users.noreply.github.com>
2018-08-24 06:53:58 -0600

    Fix Homepage

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,6 +2,7 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
 // Componentes
 
@@ -20,6 +21,10 @@ const appRoutes: Routes = [
 	{
 		path: 'register',
 		component: RegisterComponent
+	},
+	{
+		path: 'homepage',
+		component: HomepageComponent
 	}
 ];
 
